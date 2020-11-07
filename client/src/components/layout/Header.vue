d81bc89cc95190337bdca568ae2ac69e942c8d1e
MikeTheFyke <mikefyke@hotmail.com>
2020-11-06 21:16:43 -0500

    Started header styles

--- client/src/components/layout/Header.vue
+++ client/src/components/layout/Header.vue
@@ -2,9 +2,9 @@
     <div>
         <div id="header-bar">
             <button id="header-button">
-                <span class="header-span" id="header-span-top"></span>
-                <span class="header-span" id="header-span-middle"></span>
-                <span class="header-span" id="header-span-bottom"></span>
+                <div class="header-div" id="header-div-top"></div>
+                <div class="header-div" id="header-div-middle"></div>
+                <div class="header-div" id="header-div-bottom"></div>
             </button>
         </div>
     </div>
@@ -19,15 +19,37 @@ export default {
 <style scoped>
 
 #header-bar{
-
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 10%;
+    height: 100vh;
+    background-color: #20203a;
+    padding: 20px;
+    text-align: center;
 }
 
 #header-button{
-
+    position: relative;
+    top: 0px;
+    width: 50px;
+    height: 50px;
+    background-color: transparent;
+    border-color: transparent;
+    outline: none;
+    padding: 0px;
 }
 
-.header-span{
-    
+.header-div{
+    width: 100%;
+    height: 10px;
+    border-radius: 5px;
+    background-color: #f20000;;
+    margin-bottom: 5px;
+}
+
+#header-div-bottom{
+    width: 75%;
 }
 
 </style>
