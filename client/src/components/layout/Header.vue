74752d91a4f95640f20a94f7d94e746e5ea96fa8
MikeTheFyke <mikefyke@hotmail.com>
2020-11-06 21:02:09 -0500

    Added simple header boilerplate

---
+++ client/src/components/layout/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div id="header-bar">
+            <button id="header-button">
+                <span class="header-span" id="header-span-top"></span>
+                <span class="header-span" id="header-span-middle"></span>
+                <span class="header-span" id="header-span-bottom"></span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+#header-bar{
+
+}
+
+#header-button{
+
+}
+
+.header-span{
+    
+}
+
+</style>
