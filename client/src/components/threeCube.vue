7ac08cd3f99ba958b3cee2d5a9a1ebc769dfbd0a
MikeTheFyke <mikefyke@hotmail.com>
2020-11-09 01:23:36 -0500

    Added back wall

--- client/src/components/threeCube.vue
+++ client/src/components/threeCube.vue
@@ -34,15 +34,16 @@ export default {
         this.mouse = new Three.Vector2();
 
 //////// Cubes
+for (var a = 0; a <= 1; a++){
         let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2,);
-        let material = new Three.MeshNormalMaterial( { color: 0xc2b0ae, wireframe: true } );
+        let material = new Three.MeshNormalMaterial( { wireframe: true } );
 
         for (var  i = 0; i < 7; i++){
             for (var j = 0; j < 4; j++){
             var mesh = new Three.Mesh(geometry, material);
             mesh.position.x = (-1.25) + (j * 1); 
             mesh.position.y = (-1 + (i * .215));
-            mesh.position.z = (3);
+            mesh.position.z = (3 - (a));
             this.scene.add(mesh);
             }
         }
@@ -55,32 +56,33 @@ export default {
                     var mesh2 = new Three.Mesh(geometry2, material);
                     mesh2.position.x = (-1.05 + (l * 0.41) + (m * 2.25)); 
                     mesh2.position.y = (-1 + (k * 0.41));
-                    mesh2.position.z = (2.5);
+                    mesh2.position.z = (2.5 - (a));
                     this.scene.add(mesh2);
                 }
             }
         }
 //////// Top Cubes
         let geometry3 = new Three.BoxGeometry(0.3, 0.3, 0.3);
-        for (var n = 0; n <= 4; n++){
+        for (var n = 0; n < 4; n++){
             var mesh3 = new Three.Mesh(geometry3, material);
-            mesh3.position.x = (-2.25 + (n * 1)); 
+            mesh3.position.x = (-1.25 + (n * 1)); 
             mesh3.position.y = (0.55);
-            mesh3.position.z = (3);
+            mesh3.position.z = (3 - (a));
             this.scene.add(mesh3);
         }
-////////
+//////// Mini Top Cubes
         let geometry4 = new Three.BoxGeometry(0.1, 0.1, 0.1);
         for (var o = 0; o < 4; o++){
-            for (var p = 0; p < 2; p++){
+            for (var p = 0; p < 3; p++){
             var mesh4 = new Three.Mesh(geometry4, material);
-            mesh4.position.x = (-1.12 + (o * .2) + (p * 2.2)); 
+            mesh4.position.x = (-1.12 + (o * .2) + (p * 1.1)); 
             mesh4.position.y = (0.49);
-            mesh4.position.z = (2.65);
+            mesh4.position.z = (2.65 - (a));
             this.scene.add(mesh4);
             }
         }
 ////////
+}
         this.light = new Three.PointLight (0xFFFFFF, 1, 500); // Color (white), Intensity, Distance
         this.light.position.set(0,30,0); //light.position.set(10,0,25)
         this.scene.add(this.light);
