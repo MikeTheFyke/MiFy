33d825085acc06660cea8744d5a2788e41714f6f
MikeTheFyke <mikefyke@hotmail.com>
2020-11-09 16:23:15 -0500

    Started to add stylizing and templating to ProSno

--- client/src/components/ProSno.vue
+++ client/src/components/ProSno.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        
+        <div id="SnoFlake"></div>
     </div>
 </template>
 
@@ -11,5 +11,14 @@ export default {
 </script>
 
 <style scoped>
+
+#SnoFlake{
+    position: absolute;
+    top: 50px;
+    left: 50vw;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+}
 
 </style>
