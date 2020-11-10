eb72520c6c16b694bf13bb7cd49f600f03083bb4
MikeTheFyke <mikefyke@hotmail.com>
2020-11-10 12:28:14 -0500

    started snowing logic

--- client/src/components/ProSno.vue
+++ client/src/components/ProSno.vue
@@ -16,31 +16,53 @@ export default {
         mounted: function () {
             this.ctx = document.getElementById('snowing-canvas').getContext('2d');
             this.fillCanvas(this.ctx);
+            this.snowing(this.ctx);
             // document.getElementById('snowing-canvas').addEventListener('mouseover', function(){ gsap.to('#snowing-canvas', 0.75, { x: "-300px" } ); });
             // document.getElementById('snowing-canvas').addEventListener('mouseout', function(){ gsap.to('#snowing-canvas', 0.75, { x: 0 } ); });
         },
         methods: {
                 fillCanvas (ctx){
-                ctx.fillStyle = '#c4bead';
-                ctx.fillRect(0,0,300,150);
-
                 
-        }
+                ctx.fillStyle = '#a9b1c4';
+                ctx.fillRect(0,0,300,150);
+                
+                ctx.fillStyle = '#c4bead';
+                ctx.fillRect(0,125,300,25);
+        },
+            snowing (ctx) {
+
+                for( var i = 0; i <= 100; i++){
+                    ctx.fillStyle = '#c4bead';
+                    ctx.beginPath();
+                    ctx.arc(25, i, 10, 0 ,2*Math.PI);
+                    ctx.fill();
+                    
+                    ctx.fillStyle = '#a9b1c4';
+                    ctx.beginPath();
+                    ctx.arc(25, i, 10, 0 ,2*Math.PI);
+                    ctx.fill();
+                }
+            }
     },
 }
 </script>
 
 <style scoped>
 
-#snowing-canvas{
+#SnoFlakes{
     position: absolute;
     top: 100px;
     left: 300px;
+    background-color: #20203a;
+    width: 300px;
+    height: 200px;
+    text-align: center;
+}
+
+#snowing-canvas{
     width: 150px;
     height: 100px;
-    border-color: #20203a;
-    border-style: solid;
-    border-width: 5px;
+    margin-top: 50px;
 }
 
 </style>
