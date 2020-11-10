f9747cc3ca3e66b9f6c63a59198a31d80d4f11ae
MikeTheFyke <mikefyke@hotmail.com>
2020-11-09 21:39:31 -0500

    Started creating snow canvas

--- client/src/components/ProSno.vue
+++ client/src/components/ProSno.vue
@@ -1,25 +1,42 @@
 <template>
-    <div>
-        <div id="SnoFlake"></div>
+    <div id="SnoFlakes">
+        <div id="SnoFlake">
+            <canvas id="snowing-canvas"></canvas>
+        </div>
     </div>
 </template>
 
 <script>
+import gsap from "gsap";
+
 export default {
     data() {
         return{
+            ctx: "2D",
+            canvas: document.getElementById("snowing-canvas"),
         }
     }, 
         methods: {
 
         },
         mounted() {
-
+            
+            gsap.to("#SnoFlake", { duration: 5, y: '500vw', delay: 5})
         }
 }
 </script>
 
 <style scoped>
+
+#flake{
+    position: absolute;
+    top: 50px;
+    left: 30vw;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    background-color: white;
+}
 
 #SnoFlake{
     position: absolute;
