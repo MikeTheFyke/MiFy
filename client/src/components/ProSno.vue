439bc7f86ae26377b693cf21c1f182e28f6f0398
MikeTheFyke <mikefyke@hotmail.com>
2020-11-10 16:30:49 -0500

    Added logic for snowing

--- client/src/components/ProSno.vue
+++ client/src/components/ProSno.vue
@@ -6,26 +6,44 @@
 
 <script>
 // import gsap from "gsap";
-
 export default {
+    name: 'ProSno',
     data() {
         return {
         ctx: null,
         framesPerSecond : 30,
         }
     },
-        mounted: function () {
-            setInterval (function() {
-            this.ctx = document.getElementById('snowing-canvas').getContext('2d');
-                this.ctx.fillStyle = '#a9b1c4';
-                this.ctx.fillRect(0,0,300,150);
+            methods: {
+            snowing(ctx){
+                setInterval (function() {
+                    ctx.fillStyle = '#a9b1c4';
+                    ctx.fillRect(0,0,300,150);
                 
-                this.ctx.fillStyle = '#c4bead';
-                this.ctx.fillRect(0,125,300,25);
-            },1000 / this.framesPerSecond)
-        },
-        methods: {
+                    ctx.fillStyle = '#c4bead';
+                    ctx.fillRect(0,125,300,25);
 
+                    var snowY = 0;
+                    for( var i = 0; i <= 100; i++){
+                        ctx.fillStyle = '#c4bead';
+                        ctx.beginPath();
+                        ctx.arc(25, snowY + i, 10, 0 ,2*Math.PI);
+                        ctx.fill();
+                        
+                        ctx.fillStyle = '#a9b1c4';
+                        ctx.beginPath();
+                        ctx.arc(25, snowY + i, 10, 0 ,2*Math.PI);
+                        ctx.fill();
+                    }
+
+                },1000 / this.framesPerSecond)
+            }
+        },
+        mounted () {
+            
+                this.ctx = document.getElementById('snowing-canvas').getContext('2d');
+                this.snowing(this.ctx);
+            
         }
 }
 </script>
