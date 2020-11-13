65ee724a983b98e45373ba9e9ddeba67b50f7e60
MikeTheFyke <mikefyke@hotmail.com>
2020-11-13 15:59:03 -0500

    added Newvue scaffolding

---
+++ client/src/components/Newvue.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+    </div>    
+</template>
+
+<script>
+import gsap from "gsap";
+
+export default {
+    name: 'Newvue',
+    data (){
+        return{
+
+        }
+    },
+    methods : {
+
+    },
+    mounted (){
+
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
