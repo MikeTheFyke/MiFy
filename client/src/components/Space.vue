d4c89a03a848646e7ca9a55d2cf325efec72efc7
MikeTheFyke <mikefyke@hotmail.com>
2020-11-18 20:39:36 -0500

    Added styles to space container

--- client/src/components/Space.vue
+++ client/src/components/Space.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="space-container">
+        <div id="Alpha"></div>
 
     </div>
 </template>
@@ -24,5 +25,19 @@ export default {
 </script>
 
 <style scoped>
+
+#space-container{
+    position: absolute;
+    top: 200px;
+    width: 100%;
+    height: 400px;
+}
+
+#Alpha{
+    height: 100px;
+    width: 100px;
+    border-radius: 50px;
+    background-color: yellow;
+}
 
 </style>
