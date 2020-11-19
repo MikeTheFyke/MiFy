bc7ab935a68276fb637bfc9460e0c34f1b4b0ba4
MikeTheFyke <mikefyke@hotmail.com>
2020-11-18 20:37:26 -0500

    Added scafolding to space vue

---
+++ client/src/components/Space.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="space-container">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+        name: "Space",
+        data (){
+            return{
+
+            }
+        },
+        methods: {
+
+        },
+        mounted (){
+
+        }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
